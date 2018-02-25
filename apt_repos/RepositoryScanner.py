441f555051263b5a40c00c8c276327a9bcc068a8
Christoph Lutz <christoph.lutz@interface-ag.de>
2018-02-25 15:27:30 +0100

    added copyright info

--- apt_repos/RepositoryScanner.py
+++ apt_repos/RepositoryScanner.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+##################################################################################
+# Access information about binary and source packages in multiple
+# (independent) apt-repositories utilizing libapt / python-apt/
+# apt_pkg without the need to change the local system and it's apt-setup.
+#
+# Copyright (C) 2018  Christoph Lutz
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+##################################################################################
+
 import sys
 import configparser
 import urllib3
